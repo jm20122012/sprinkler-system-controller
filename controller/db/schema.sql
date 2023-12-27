98755adecfa5dc32968ddd89d0f22e45fc9d9e58
josh <josh@pop-os.localdomain>
2023-12-26 19:35:04 -0700

    updated schema file

--- controller/db/schema.sql
+++ controller/db/schema.sql
@@ -4,11 +4,27 @@ CREATE TABLE IF NOT EXISTS zone_list (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS weekdays (
+    id SERIAL NOT NULL,
+    day_label VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)    
+);
+
+INSERT INTO weekdays (day_label) VALUES ('Sunday');
+INSERT INTO weekdays (day_label) VALUES ('Monday');
+INSERT INTO weekdays (day_label) VALUES ('Tuesday');
+INSERT INTO weekdays (day_label) VALUES ('Wednesday');
+INSERT INTO weekdays (day_label) VALUES ('Thursday');
+INSERT INTO weekdays (day_label) VALUES ('Friday');
+INSERT INTO weekdays (day_label) VALUES ('Saturday');
+
 CREATE TABLE IF NOT EXISTS event_schedule (
     id SERIAL NOT NULL,
+    zone_id INT NOT NULL,
     start_time TIME NOT NULL,
     duration_minutes INT NOT NULL,
     day_of_week INT NOT NULL,
-    event_members TEXT[] NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (zone_id) REFERENCES zone_list(id),
+    FOREIGN KEY (day_of_week) REFERENCES weekdays(id)
 );
