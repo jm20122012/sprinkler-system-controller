b8f048abe61df26cd20c5c6e3a9ed85e80626bee
jm <josh@pop-os.localdomain>
2024-01-04 18:45:56 -0700

    Broke manual control into its own component

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,8 +1,12 @@
 import MainContent from './components/MainContent.jsx';
 import Navbar from "./components/Navbar.jsx";
-
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
 
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
 
--- ui/src/components/ZoneStatusItem.jsx
+++ ui/src/components/ZoneStatusItem.jsx
@@ -1,20 +1,62 @@
 import React, {useState} from 'react'
 import {Row, Col} from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
-import { useSelector } from 'react-redux';
-
 import "../stylesheets/ZoneStatusItem.css";
 
+const DURATION_MIN = 1;
+const DURATION_MAX = 60;
+
 const ZoneStatusItem = (props) => {
+
+    console.log("Zone status item rendered");
+    return (
+        <>
+            <div className="zone-status-item-container">
+                <div className="zone-status-item-info-container">
+                    <ZoneStatusInfoRow label="Zone Label" value={props.zoneLabel} />
+                    <ZoneStatusInfoRow label="Zone Status" value="On" />
+                    <ZoneStatusInfoRow label="Next Event" value="13:00" />
+                    <hr style={{"width": "80%"}}></hr>
+                    <ManualControlComponent zoneLabel={props.zoneLabel}/>
+                </div>
+            </div>
+        </>
+    )
+}
+
+const ZoneStatusInfoRow = (props) => {
+    console.log("Zone status info row rendered")
+    return (
+        <>
+            <Row style={{"width": "100%"}}>
+                <Col>
+                    <p>{props.label}</p>
+                </Col>
+                <Col>
+                    <p>{props.value}</p>
+                </Col>
+            </Row>
+        </>
+    )
+};
+
+const ManualControlComponent = (props) => {
     const [manualControlSwitch, setManualControlSwitch] = useState(false);
-    const [manualControlMinutes, setManualControlMinutes] = useState(1);
+    const [manualControlDuration, setManualControlDuration] = useState(0);
     
+    // TODO: Change this to hit the actual endpoint with
+    // query params for zone label
+    const checkZoneReady = async () => {
+        const response = await fetch(`http://localhost:3000/zoneReady`);
+        const data = await response.json();
+        return data[props.zoneLabel];
+    }
+
     const handleManualControlToggle = async (checked) => {
         console.log("Manual control toggled to: ", checked);
-        console.log("Manual control minutes: ", manualControlMinutes);
+        console.log("Manual control duration: ", manualControlDuration);
         if (checked) {
             console.log("Checking if zone is ready for manual control...")
             const zoneReady = await checkZoneReady();
@@ -30,65 +72,34 @@ const ZoneStatusItem = (props) => {
         }
     };
 
-    // TODO: Change this to hit the actual endpoint with
-    // query params for zone label
-    const checkZoneReady = async () => {
-        const response = await fetch(`http://localhost:3000/zoneReady`);
-        const data = await response.json();
-        return data[props.zoneLabel];
-    }
-
+    console.log("Manual control component rendered");
     return (
         <>
-            <div className="zone-status-item-container">
-                <div className="zone-status-item-info-container">
-                    <ZoneStatusInfoRow label="Zone Label" value={props.zoneLabel} />
-                    <ZoneStatusInfoRow label="Zone Status" value="On" />
-                    <ZoneStatusInfoRow label="Next Event" value="13:00" />
-                    <p>Manual Control</p>
-                    <Row className="align-items-center" style={{"width": "100%"}}>
-                        <Col>
-                            <InputGroup>
-                                <Form.Check
-                                    type="switch"
-                                    id={`${props.zoneLabel}-manual-control-switch`}
-                                    label="Off/On"
-                                    checked={manualControlSwitch}
-                                    onChange={(e) => handleManualControlToggle(e.target.checked)}
-                                />
-                            </InputGroup>
-                        </Col>
-                        <Col>
-                            <InputGroup>
-                                <Form.Control
-                                    type="number"
-                                    min="1"
-                                    max="60"
-                                    placeholder="Duration (minutes)"
-                                    aria-label="Duration"
-                                    aria-describedby="basic-addon2"
-                                    onChange={(e) => setManualControlMinutes(e.target.value)}
-                                />
-                            </InputGroup>
-                        </Col>
-                    </Row>
-                </div>
-            </div>
-        </>
-    )
-}
-
-const ZoneStatusInfoRow = (props) => {
-    return (
-        <>
-            <Row style={{"width": "100%"}}>
-                <Col>
-                    <p>{props.label}</p>
-                </Col>
-                <Col>
-                    <p>{props.value}</p>
-                </Col>
-            </Row>
+            <p>Manual Control</p>
+            <InputGroup 
+                className="align-items-center">
+                <Form.Check
+                    type="switch"
+                    id={`${props.zoneLabel}-manual-control-switch`}
+                    label="Off/On"
+                    checked={manualControlSwitch}
+                    onChange={(e) => handleManualControlToggle(e.target.checked)}
+                    disabled={manualControlDuration === 0 ? true : false}
+                    style={{"marginRight": "10px"}}
+                />
+                <Form.Select
+                    value={manualControlDuration}
+                    onChange={(e) => setManualControlDuration(Number(e.target.value))}>
+                    <option value={0}>Select Duration</option>
+                    {
+                        Array.from(Array(DURATION_MAX - DURATION_MIN + 1).keys()).map((i) => {
+                            return (
+                                <option key={i} value={i + DURATION_MIN}>{i + DURATION_MIN}</option>
+                            )
+                        })
+                    }
+                </Form.Select>
+            </InputGroup>
         </>
     )
 };
